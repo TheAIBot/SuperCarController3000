af94c8233ba4ae3412b3b749a3841cb261723f0e
TheAIBot <andreasuranus@hotmail.com>
2017-11-11 15:35:07 +0100

    simplified barrier implementation

--- src/Barrier.java
+++ src/Barrier.java
@@ -1,47 +1,27 @@
 public class Barrier {
 	
-	private boolean isOn = false; 
+	private boolean isOn = false;
 	private boolean doShutdown = false;
-	private int numberCarsAtBarrier = 0;
-	private boolean isShutdownDone  = false;
-	private boolean exitBarrier = false;
+	private int carsInBarrier = 0;
+	private boolean roundState = true;
 
 	public synchronized void sync() throws InterruptedException {
-		//new cars can't enter the barrier 
-		//because they won't get notified 
-		//to leave
-		if (exitBarrier) {
-			while (exitBarrier) {
-				wait();
-			}
-		}
-
-		//only enter barrier if it's on
-		if (!isOn && !doShutdown) {
+		if (!isOn) {
 			return;
 		}
 
-		numberCarsAtBarrier++;
-		//if all cars are at the barrier then release them all
-		if (numberCarsAtBarrier == CarControl.NUMBER_OF_CARS) {
-			boolean oldDoShutdown = doShutdown;
-			off();
-			isOn = !oldDoShutdown;
-		}
+		final boolean carState = roundState;
 
-		//wait at barrier until all cars are at the barrier
-		while (!exitBarrier) {
-			wait();
-		}
-
-		numberCarsAtBarrier--;		
-		//if this is the last car leaving the barrier 
-		//then allow cars to enter the barrier again
-		if (numberCarsAtBarrier == 0) {
-			exitBarrier = false;
-			//there might be threads waiting to enter 
-			//that are sleeping in the top wait call
+		carsInBarrier++;
+		if (carsInBarrier == CarControl.NUMBER_OF_CARS) {
+			roundState = !roundState;
+			isOn = !doShutdown;
+			carsInBarrier = 0;
 			notifyAll();
+		}
+
+		while (carState == roundState) {
+			wait();
 		}
 	}
 	
@@ -52,26 +32,24 @@ public class Barrier {
 	
 	public synchronized void off() {
 		isOn = false;
-		doShutdown = false;
-		isShutdownDone = true;
-		if (numberCarsAtBarrier > 0) {
-			exitBarrier = true;
+
+		if (carsInBarrier > 0) {
+			roundState = !roundState;
+			carsInBarrier = 0;
 			notifyAll();
 		}
 	}
 
 	public synchronized void shutdown() throws InterruptedException {
-		if (isOn && numberCarsAtBarrier > 0) {
+		if (carsInBarrier > 0) {
 			doShutdown = true;
-			isShutdownDone = false;
-			//wait for all cars to begin exiting the barrier.
-			//exitBarrier can't be used here because it won't wait 
-			//for this thread to go out of this function before
-			//it's set back to false
-			while (!isShutdownDone)
-			{
+			final boolean carState = roundState;
+			while (carState == roundState) {
 				wait();
 			}
+		}
+		else {
+			isOn = false;
 		}
 	}
 }
