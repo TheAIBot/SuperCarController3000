b598f52f5f7b4f0c00da0c0e8034dd60f1dea8e0
TheAIBot <andreasuranus@hotmail.com>
2017-11-21 20:08:31 +0100

    added comments and fixed bug

--- src/Barrier.java
+++ src/Barrier.java
@@ -1,21 +1,35 @@
 public class Barrier {
-	
+
+	//is true if the barrier is turned on
 	private boolean isOn = false;
+	//is true if the barrier is currently shutting down
 	private boolean doShutdown = false;
+	//count of how many cars are in the barrier
 	private int carsInBarrier = 0;
+	//count of how many cars has to wake up
+	private int carsToWakeUp = 0;
 	private boolean roundState = true;
 
 	public synchronized void sync() throws InterruptedException {
+		//i the barrier is offline
+		//then go through the barrier
 		if (!isOn) {
 			return;
 		}
 
+		//opy to local, so it can be checked
+		//later if roundState has changed value
 		final boolean carState = roundState;
 
 		carsInBarrier++;
+		//if this is the last car to enter the barrier
+		//then change roundState and wake up all the waiting
+		//Cars so they can leave the barrier
 		if (carsInBarrier == CarControl.NUMBER_OF_CARS) {
 			roundState = !roundState;
+			//if shutting down then turn off the barrier
 			isOn = !doShutdown;
+			carsToWakeUp = carsInBarrier;
 			carsInBarrier = 0;
 			notifyAll();
 		}
@@ -23,9 +37,29 @@ public class Barrier {
 		while (carState == roundState) {
 			wait();
 		}
+
+		carsToWakeUp--;
+		//if this is the last car out of the barrier
+		//then notify all threads because
+		//on() might be waiting for all threads
+		//to leave the barrier
+		if(carsToWakeUp == 0){
+			notifyAll();
+		}
 	}
 	
 	public synchronized void on() {
+		//can't enter the barrier while
+		//cars are leaving the barrier
+		//because that could lead to a deadlock
+		//if off is called right after
+		while (carsToWakeUp > 0){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		isOn = true;
 		doShutdown = false;
 	}
@@ -33,17 +67,26 @@ public class Barrier {
 	public synchronized void off() {
 		isOn = false;
 
+		//if any cars are in the barrier
+		//then release the cars
 		if (carsInBarrier > 0) {
 			roundState = !roundState;
+			carsToWakeUp = carsInBarrier;
 			carsInBarrier = 0;
 			notifyAll();
 		}
 	}
 
 	public synchronized void shutdown() throws InterruptedException {
+		//can only shutdown if there are cars
+		//in the barrier
 		if (carsInBarrier > 0) {
 			doShutdown = true;
 			final boolean carState = roundState;
+			//wait for all cars to leave the barrier
+			//before retuning again because
+			//that's what the gui require in order
+			//to show the shutdown on the screen
 			while (carState == roundState) {
 				wait();
 			}
--- src/CriticalRegion.java
+++ src/CriticalRegion.java
@@ -1,16 +1,27 @@
 public class CriticalRegion {
+	//is a counter of how many cars are in the alley
+	//and which direction they are from.
+	//if there are 3 cars in the alley and they are
+	//from the down direction, then the value of
+	//the variable is -3. If there are 3 cars from up
+	//then the value is 3.
+	//there are 0 cars in the alley when the value is 0.
 	private int dirCarCount = 0;
 	private static final int UP = 1;
 	private static final int DOWN = -1;
 	
 	
 	public synchronized void enter(int carNumber) throws InterruptedException {
+		//get direction from the car number
 		final int dir = (carNumber  <= 4) ? UP : DOWN;
 
+		//while there are cars from the other direction
+		//wait for them all to leave
 		while (hasToWait(dir)) {
 			wait();
 		}
 
+		//enter the alley by adding the direction value
 		dirCarCount += dir;
 	}
 
@@ -18,15 +29,21 @@ public class CriticalRegion {
 	{
 		//direction is negative then dirCarCount
 		//also has to be 0 or negative for the car to continue.
-		//Other way around with posetive.
+		//Other way around with positive.
 		return dirCarCount * dir < 0;
 	}
 
 	public synchronized void leave(int carNumber)  {
+		//get direction from the car number
 		final int dir = (carNumber  <= 4) ? UP : DOWN;
 
+		//leave alley
 		dirCarCount -= dir;
 
+		//if there are now no cars in the alley
+		//then notify all other cars, that are
+		//waiting to enter the alley, that they
+		//can now try to enter the alle again
 		if (dirCarCount == 0) {
 			notifyAll();
 		}
